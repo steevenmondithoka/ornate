5a0889d0dee9f9ad70143ffb69fe4177c2729a60
steevenmondithoka <steevemondithoka@gmail.com>
2026-02-06 21:31:29 +0530

    added frontend urls

--- server/server.js
+++ server/server.js
@@ -15,11 +15,28 @@ const adminRoutes = require('./routes/adminRoutes');
 
 const app = express();
 
+
+
 // Connect to Database
 connectDB();
 
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173", // For local testing
+  process.env.FRONTEND_URL  // For production
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // Allow requests with no origin (like mobile apps or curl)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return callback(new Error("CORS policy violation"), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
